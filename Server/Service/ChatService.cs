e2372dfc4d01bec407162e900599308285b6542f
Niemeijeren <niemeijeren@gmail.com>
2018-10-23 13:27:29 +0200

    CSHARPDOC

--- Server/Service/ChatService.cs
+++ Server/Service/ChatService.cs
@@ -24,7 +24,12 @@ namespace Server.Service
         }
 
 
-        //Method for adding a new user to a chat
+        /// <summary>
+        /// Method for adding a new user to a chat
+        /// </summary>
+        /// <returns>The users to chat async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
+        /// <param name="users">Users.</param>
         public async Task<bool> AddUsersToChatAsync(int chatId, params ApplicationUser[] users)
         {
             var chat = await _chats.FindAsync(chatId);
@@ -46,7 +51,11 @@ namespace Server.Service
         }
 
 
-        //Method for finding out wether a chat is active or not (has active users)
+        /// <summary>
+        /// Method for finding out wether a chat is active or not (has active users)
+        /// </summary>
+        /// <returns>The is active async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
         public async Task<bool> ChatIsActiveAsync(int chatId)
         {
             var chat = await _chats.FindAsync(chatId);
@@ -58,7 +67,11 @@ namespace Server.Service
         }
 
 
-        //Method for Creating a new chat
+        /// <summary>
+        /// Method for Creating a new chat
+        /// </summary>
+        /// <returns>The chat async.</returns>
+        /// <param name="chat">Chat.</param>
         public async Task<bool> CreateChatAsync(Chat chat)
         {
             _chats.Add(chat);
@@ -72,7 +85,12 @@ namespace Server.Service
         }
 
 
-        //Method for inviting a user who is not in the chat
+        /// <summary>
+        /// Method for inviting a user who is not in the chat
+        /// </summary>
+        /// <returns>The to chat async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
+        /// <param name="user">User.</param>
         public async Task<bool> InviteToChatAsync(int chatId, ApplicationUser user)
         {
             var chat = await _chats.FindAsync(chatId);
@@ -91,7 +109,12 @@ namespace Server.Service
         }
 
 
-        //Method for deleting/removing a chat for a specific user
+        /// <summary>
+        /// Method for deleting/removing a chat for a specific user
+        /// </summary>
+        /// <returns>The chat async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
+        /// <param name="user">User.</param>
         public async Task<bool> RemoveChatAsync(int chatId, ApplicationUser user)
         {
             var chat = await _chats.FindAsync(chatId);
@@ -109,7 +132,12 @@ namespace Server.Service
         }
 
 
-        //Method for removing a user from a specific chat
+        /// <summary>
+        /// Method for removing a user from a specific chat
+        /// </summary>
+        /// <returns>The users from chat async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
+        /// <param name="users">Users.</param>
         public async Task<bool> RemoveUsersFromChatAsync(int chatId, params ApplicationUser[] users)
         {
             var chat = await _chats.FindAsync(chatId);
@@ -135,7 +163,11 @@ namespace Server.Service
         }
 
 
-        //Method for retrieving list of chats that a specific user is part of
+        /// <summary>
+        /// Method for retrieving list of chats that a specific user is part of
+        /// </summary>
+        /// <returns>The chats async.</returns>
+        /// <param name="user">User.</param>
         public async Task<ICollection<Chat>> RetrieveChatsAsync(ApplicationUser user)
         {
             return await _chats.Cast<Chat>().Where(c => c.ApplicationUsers.Contains(user)).ToListAsync();
@@ -148,7 +180,12 @@ namespace Server.Service
         }
 
 
-        //Method for sending a message in a chat
+        /// <summary>
+        /// Method for sending a message in a chat
+        /// </summary>
+        /// <returns>The message async.</returns>
+        /// <param name="chatId">Chat identifier.</param>
+        /// <param name="message">Message.</param>
         public async Task<bool> SendMessageAsync(int chatId, Message message)
         {
             var chat = await _chats.FindAsync(chatId);
