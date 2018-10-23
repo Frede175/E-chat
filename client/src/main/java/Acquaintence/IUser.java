41f2cc6a6e75a9e463cb50dada601b5dcbde635c
Lars Jørgensen <lars.b.jn@gmail.com>
2018-10-23 12:37:30 +0200

    * Added User and interface for user
    * Added User and interface for user

---
+++ client/src/main/java/Acquaintence/IUser.java
@@ -0,0 +1,7 @@
+package Acquaintence;
+
+public interface IUser {
+    int getID();
+    String getName();
+    void setName(String name);
+}
---
+++ client/src/main/java/Business/Models/User.java
@@ -0,0 +1,29 @@
+package Business.Models;
+
+import Acquaintence.IUser;
+
+public class User implements IUser {
+
+    private int ID;
+    private String name;
+
+    public User(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+}
