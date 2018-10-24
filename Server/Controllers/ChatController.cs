591acf7adc595817c2d430b491b22ceb98e4d29a
Niemeijeren <niemeijeren@gmail.com>
2018-10-24 14:03:20 +0200

    Chatcontroller basics

---
+++ Server/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Context;
+using Server.Service.Interfaces;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class ChatController
+    {
+        private readonly IChatService _chatService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+
+
+        public ChatController(IChatService chatService, , UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _chatService = chatService;
+
+        }
+
+
+    }
+}
