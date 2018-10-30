b4d871c3a646376590bc7b02abea515bf51ded61
Niemeijeren <niemeijeren@gmail.com>
2018-10-30 13:26:47 +0100

    DB initializer

--- Server/Context/ApplicationDbInitializer.cs
+++ Server/Context/ApplicationDbInitializer.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Server.Context
 {
     //Not working atm.
     public static class ApplicationDbInitializer
     {
-        public static async Task SeedUsersAsync(UserManager<ApplicationUser> userManager) {
+        public static async Task SeedUsersAsync(IServiceProvider services) {
 
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
             var user = await userManager.FindByNameAsync("admin");
 
             if (user == null) {
--- Server/Security/PermissionsAuthorizationHandler.cs
+++ Server/Security/PermissionsAuthorizationHandler.cs
@@ -13,7 +13,8 @@ namespace Server.Security
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionsAuthorizationRequirement requirement)
         {
-            if (!requirement.RequiredPermissions.Select(r => r.ToString()).Except(context.User.Claims.Select(c => c.Type)).Any()){
+            if (!requirement.RequiredPermissions.Select(r => r.ToString()).Except(context.User.Claims.Select(c => c.Type)).Any())
+            {
                 context.Succeed(requirement);
             }
             return Task.CompletedTask;
