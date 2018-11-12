4d574ac9f22b3d445dd98169ab1cb5abc05bfd90
Lars Jørgensen <lars.b.jn@gmail.com>
2018-11-12 15:11:03 +0100

    Refactored chatbox name
    * Refactored chatbox name

--- client/src/main/java/GUI/Controller/MessageViewController.java
+++ client/src/main/java/GUI/Controller/MessageViewController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.ListView;
 import java.util.List;
 
 
-public class ChatBoxController{
+public class MessageViewController {
 
     @FXML
     public ListView<IMessageIn> chatBox;
