e8717e5fd17a6ca9bffcbe3d6eeb71822af0c9c8
Lars Jørgensen <lars.b.jn@gmail.com>
2018-11-13 10:49:17 +0100

    Removed setCurrentChat from IBusinessFacade
    * Removed setCurrentChat from IBusinessFacade

--- client/src/main/java/Acquaintence/IBusinessFacade.java
+++ client/src/main/java/Acquaintence/IBusinessFacade.java
@@ -5,19 +5,11 @@ import Business.Connection.RequestResponse;
 import java.util.List;
 
 public interface IBusinessFacade {
-
     ConnectionState login(String username, String password);
     RequestResponse<List<? extends IChat>> getChats();
-
     void sendMessage(String message);
-
-
     RequestResponse<List<? extends IMessageIn>> getMessages();
-
     RequestResponse<List<? extends IUser>> getUsersInChat();
     RequestResponse<List<? extends IDepartment>> getDepartments();
-
     IChat getCurrentChat();
-    //void setCurrentChat(int chatID);
-
 }
