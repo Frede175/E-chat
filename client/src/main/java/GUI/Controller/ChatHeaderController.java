1f0afc49e291aa0e1a57833bbefc3b93f23f37e1
Kristian <kkwowm@gmail.com>
2018-11-13 11:29:49 +0100

    fixed shit

--- client/src/main/java/GUI/Controller/ChatHeaderController.java
+++ client/src/main/java/GUI/Controller/ChatHeaderController.java
@@ -1,18 +1,15 @@
 package GUI.Controller;
 
-import GUI.PopUpWindow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
-import java.io.IOException;
 
 public class ChatHeaderController {
 
     @FXML
     private Label chatNameL;
 
-    public void usersInChat(ActionEvent actionEvent) throws IOException {
-        PopUpWindow.createUserPopUp("/fxml/CreateUser.fxml");
+    public void usersInChat(ActionEvent actionEvent) {
+        chatNameL.setText("Lars er mega nice og sej");
     }
 }
