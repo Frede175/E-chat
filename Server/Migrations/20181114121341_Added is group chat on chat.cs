233795335be959cffe542aba8c53665746d794b8
Frederik Rosenberg <fsr1998@hotmail.com>
2018-11-14 13:17:08 +0100

    Added colunm IsgroupChat to chat

---
+++ Server/Migrations/20181114121341_Added is group chat on chat.cs
@@ -0,0 +1,359 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Server.Migrations
+{
+    public partial class Addedisgroupchatonchat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Chat_ChatId",
+                table: "Message");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictTokens_ApplicationId",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictAuthorizations_ApplicationId",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictTokens",
+                maxLength: 450,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictTokens",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferenceId",
+                table: "OpenIddictTokens",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictTokens",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "OpenIddictScopes",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictScopes",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictAuthorizations",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictAuthorizations",
+                maxLength: 450,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictAuthorizations",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictAuthorizations",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictApplications",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictApplications",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "OpenIddictApplications",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChatId",
+                table: "Message",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGroupChat",
+                table: "Chat",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_ApplicationId_Status_Subject_Type",
+                table: "OpenIddictTokens",
+                columns: new[] { "ApplicationId", "Status", "Subject", "Type" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictAuthorizations_ApplicationId_Status_Subject_Type",
+                table: "OpenIddictAuthorizations",
+                columns: new[] { "ApplicationId", "Status", "Subject", "Type" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Chat_ChatId",
+                table: "Message",
+                column: "ChatId",
+                principalTable: "Chat",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Chat_ChatId",
+                table: "Message");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictTokens_ApplicationId_Status_Subject_Type",
+                table: "OpenIddictTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OpenIddictAuthorizations_ApplicationId_Status_Subject_Type",
+                table: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropColumn(
+                name: "IsGroupChat",
+                table: "Chat");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 450);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictTokens",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReferenceId",
+                table: "OpenIddictTokens",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictTokens",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "OpenIddictScopes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictScopes",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 450);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OpenIddictAuthorizations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictAuthorizations",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictApplications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConcurrencyToken",
+                table: "OpenIddictApplications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "OpenIddictApplications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChatId",
+                table: "Message",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictTokens_ApplicationId",
+                table: "OpenIddictTokens",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenIddictAuthorizations_ApplicationId",
+                table: "OpenIddictAuthorizations",
+                column: "ApplicationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Chat_ChatId",
+                table: "Message",
+                column: "ChatId",
+                principalTable: "Chat",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
