74e32a650a2596a09a106537b98800f9c5ab0f65
Lars Jørgensen <lars.b.jn@gmail.com>
2018-11-19 16:21:04 +0100

    Revert "Fixed changeName function error"
    This reverts commit 8ce78c0100de450d8cb45a68d444850a74c543f3.

--- client/src/main/java/GUI/Controller/ChatHeaderController.java
+++ client/src/main/java/GUI/Controller/ChatHeaderController.java
@@ -35,10 +35,6 @@ public class ChatHeaderController {
     @FXML
     private Button cHButton;
 
-    private void changeName(ChangeChatEvent event) {
-        chatNameL.setText(event.getChat().getName());
-    }
-
 
     public void usersInChatBtn(ActionEvent actionEvent) throws IOException {
 
