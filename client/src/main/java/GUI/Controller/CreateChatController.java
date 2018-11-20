50b3629e4e5cbd8bbf838785d4d9c320a7c5901c
Frederik Sørensen <soerensen5492@gmail.com>
2018-11-20 11:26:33 +0100

    urrrgrhrgh

---
+++ client/src/main/java/GUI/Controller/CreateChatController.java
@@ -0,0 +1,31 @@
+package GUI.Controller;
+
+
+import Acquaintence.IDepartment;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class CreateChatController {
+
+
+    public TextField nameTextField;
+    public ChoiceBox choiceBox;
+
+    public void initialize(){
+
+        choiceBox.getItems().addAll(IDepartment);
+
+    }
+
+
+    public void createChat(ActionEvent actionEvent){
+        GUI.GUI.getInstance().getBusiness().createChat(nameTextField.getText(), 1);
+
+    }
+
+
+    private void getChoice(ChoiceBox<IDepartment> choiceBox){
+
+    }
+}
