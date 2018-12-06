dd40a6a410c67cab7db359b6e69c87977f5f92f9
Frederik Rosenberg <fsr1998@hotmail.com>
2018-12-06 17:46:08 +0100

    Revert "Upgraded server to dotnet version 2.2"
    This reverts commit 1c4546954a0c6b6c3b6c57b244901d0ff7740bad.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -126,7 +127,7 @@ namespace Server
             services.AddSingleton<IAuthorizationHandler, PermissionsAuthorizationHandler>();
             services.AddSingleton(typeof(IHubState<,>), typeof(HubState<,>));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(options =>
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options => 
             {
                 options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
             });
